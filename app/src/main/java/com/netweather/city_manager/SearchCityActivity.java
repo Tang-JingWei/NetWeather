f026424e9004a26ee590a8cca6c61237b77112e1
“tangweijin” <2752888902@qq.com>
2022-11-13 09:42:51 +0800

    课设4--安卓(天气应用)

---
+++ app/src/main/java/com/netweather/city_manager/SearchCityActivity.java
@@ -0,0 +1,104 @@
+package com.netweather.city_manager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.*;
+import com.google.gson.Gson;
+import com.netweather.MainActivity;
+import com.netweather.R;
+import com.netweather.base.BaseActivity;
+import com.netweather.bean.WeatherBean;
+
+public class SearchCityActivity extends BaseActivity implements View.OnClickListener{
+    EditText searchEt;
+    ImageView submitIv;
+    GridView searchGv;
+    String[]hotCitys = {"北京","上海","广州","深圳","珠海","佛山","南京","苏州","厦门",
+    "长沙","成都","福州","杭州","武汉","青岛","西安","太原","沈阳","重庆","天津","南宁"};
+    private ArrayAdapter<String> adapter;
+    String url1 = "https://wis.qq.com/weather/common?source=pc&weather_type=observe|index|rise|alarm|air|tips|forecast_24h&province=";
+    String url2 = "&city=";
+    String city;
+    String provice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_city);
+        searchEt = findViewById(R.id.search_et);
+        submitIv = findViewById(R.id.search_iv_submit);
+        searchGv = findViewById(R.id.search_gv);
+        submitIv.setOnClickListener(this);
+//        设置适配器
+        adapter = new ArrayAdapter<>(this, R.layout.item_hotcity, hotCitys);
+        searchGv.setAdapter(adapter);
+        setListener();
+    }
+/* 设置监听事件*/
+    private void setListener() {
+        searchGv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                city = hotCitys[position];
+//                获取省份
+                provice=GetProvice(city);
+                String url = url1+provice+url2+city;
+                loadData(url);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.search_iv_submit:
+                city = searchEt.getText().toString();
+                if (!TextUtils.isEmpty(city)) {
+//                      判断是否能够找到这个城市
+                    provice=GetProvice(city);
+                    String url = url1+provice+url2+city;
+                    loadData(url);
+                }else {
+                    Toast.makeText(this,"输入内容不能为空！",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onSuccess(String result) {
+        WeatherBean weatherBean = new Gson().fromJson(result, WeatherBean.class);
+        if (weatherBean.getData().getIndex().getClothes()!=null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+            city=provice+" "+city;
+            intent.putExtra("city",city);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this,"暂时未收入此城市天气信息...",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String GetProvice(String city) {
+        String[]Citys = {"北京","上海","广东省 广州","广东省 深圳","广东省 珠海","广东省 佛山","湖南省 常德",
+                "江苏省 南京","江苏省 苏州","福建省 厦门","湖南省 长沙","四川省 成都","福建省 福州","浙江省 杭州",
+                "湖北省 武汉","山东省 青岛","陕西省 西安","山西省 太原","辽宁省 沈阳","重庆","天津","广西省 南宁",
+                "湖南省 株洲","湖南省 湘潭","湖南省 衡阳","湖南省 邵阳","湖南省 岳阳","湖南省 张家界","湖南省 益阳",
+                "湖南省 郴州","湖南省 永州","湖南省 怀化","湖南省 吉首"};
+        for(int i=0;i<Citys.length;i++){
+            if(Citys[i].contains(city)){
+                if(Citys[i].split(" ").length>1)
+                {   provice =Citys[i].split(" ")[0];
+                }
+                else
+                {
+                    provice = Citys[i].split(" ")[0];
+                }
+                break;
+            }
+        }
+        return provice;
+    }
+}
